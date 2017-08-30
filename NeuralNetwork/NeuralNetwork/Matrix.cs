465823b375200b4d81b0a83381e1da5b253e110d
MauriceBot <nikotammentie@gmail.com>
2017-08-30 23:30:44 +0300

    Initial upload

---
+++ NeuralNetwork/NeuralNetwork/Matrix.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Matrix
+    {
+        public const int DimSize = 3;
+        private double[,] m_matrix = new double[DimSize, DimSize];
+
+        public Matrix(int rows, int columns)
+        {
+            m_matrix = new double[rows, columns];
+        }
+
+        public static Matrix operator -(Matrix mat1, Matrix mat2)
+        {
+            int rows = mat1.m_matrix.GetLength(0);
+            int columns = mat1.m_matrix.GetLength(1);
+            Matrix newMatrix = new Matrix(rows, columns);
+
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < columns; y++)
+                    newMatrix[x, y] = mat1[x, y] - mat2[x, y];
+
+            return newMatrix;
+        }
+
+        public static Matrix operator -(double number, Matrix mat1)
+        {
+            int rows = mat1.m_matrix.GetLength(0);
+            int columns = mat1.m_matrix.GetLength(1);
+            Matrix newMatrix = new Matrix(rows, columns);
+
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < columns; y++)
+                    newMatrix[x, y] = number - mat1[x, y];
+
+            return newMatrix;
+        }
+
+        public static Matrix operator *(Matrix mat1, Matrix mat2)
+        {
+            int rows = mat1.m_matrix.GetLength(0);
+            int columns = mat1.m_matrix.GetLength(1);
+            Matrix newMatrix = new Matrix(rows, columns);
+
+            for (int x = 0; x < rows; x++)
+                for (int y = 0; y < columns; y++)
+                    newMatrix[x, y] = mat1[x, y] * mat2[x, y];
+
+            return newMatrix;
+        }
+
+        internal void TwoDimensions()
+        {
+            double[,] temp = new double[2, m_matrix.GetLength(1)];
+            for (int i = 0; i < temp.GetLength(1); i++)
+            {
+                temp[0, i] = m_matrix[0, i];
+                temp[1, i] = 1;
+            }
+            m_matrix = temp;
+
+        }
+
+        public static Matrix DotProduct(Matrix a, Matrix b)
+        {
+
+            Matrix c = new Matrix(a.m_matrix.GetLength(0), b.m_matrix.GetLength(1));
+            for (int i = 0; i < c.m_matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < c.m_matrix.GetLength(1); j++)
+                {
+                    c[i, j] = 0;
+                    for (int k = 0; k < a.m_matrix.GetLength(1); k++) // OR k<b.GetLength(0)
+                        c[i, j] = c[i, j] + a[i, k] * b[k, j];
+                }
+            }
+            return c;
+        }
+
+        public Matrix Transpose()
+        {
+            int w = m_matrix.GetLength(0);
+            int h = m_matrix.GetLength(1);
+
+            Matrix result = new Matrix(h, w);
+
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    result[j, i] = m_matrix[i, j];
+
+            return result;
+        }
+
+        public Matrix Sigmoid()
+        {
+            int w = m_matrix.GetLength(0);
+            int h = m_matrix.GetLength(1);
+
+            Matrix result = new Matrix(w, h);
+
+            for (int i = 0; i < w; i++)
+                for (int j = 0; j < h; j++)
+                    result[i, j] = 1 / (1 + Math.Exp(-m_matrix[i, j])); ;
+
+            return result;
+        }
+
+        public int GetLength(int x)
+        {
+            return m_matrix.GetLength(x);
+        }
+
+        // allow callers to initialize
+        public double this[int x, int y]
+        {
+            get { return m_matrix[x, y]; }
+            set { m_matrix[x, y] = value; }
+        }
+    }
+}
---
+++ NeuralNetwork/NeuralNetwork/NeuralNetwork.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class NeuralNetwork
+    {
+        private int inputnodes;
+        private int hiddennodes;
+        private int outputnodes;
+        private double learningrate;
+
+        Matrix weightInputHidden = new Matrix(0, 0);
+        Matrix weightHiddenOutput = new Matrix(0, 0);
+
+        public NeuralNetwork(int inputnodes, int hiddennodes, int outputnodes, double learningrate)
+        {
+            this.inputnodes = inputnodes;
+            this.hiddennodes = hiddennodes;
+            this.outputnodes = outputnodes;
+            this.learningrate = learningrate;
+            weightInputHidden = RandomizeWeights(inputnodes, hiddennodes);
+            weightHiddenOutput = RandomizeWeights(hiddennodes, outputnodes);
+        }
+
+        public Matrix Query(Matrix inputs)
+        {
+            // Inputs going into the hidden layer.
+            Matrix hidden_inputs = Matrix.DotProduct(inputs, weightInputHidden);
+            // Inputs leaving the hidden layer sigmoided.
+            Matrix hidden_outputs = hidden_inputs.Sigmoid();
+
+            // Inputs going into the final layer.
+            Matrix final_inputs = Matrix.DotProduct(hidden_outputs, weightHiddenOutput);
+            // Inputs leaving the final layer sigmoided.
+            Matrix final_outputs = final_inputs.Sigmoid();
+
+            return final_outputs;
+        }
+
+        private Matrix RandomizeWeights(int r, int c)
+        {
+            Random rand = new Random();
+            Matrix temp = new Matrix(r, c);
+            for (int i = 0; i < temp.GetLength(0); i++)
+                for (int j = 0; j < temp.GetLength(1); j++)
+                    temp[i, j] = rand.NextDouble() - 0.5;
+            return temp;
+        }
+
+        public void train(Matrix inputs, Matrix target)
+        {
+            // Inputs going into the hidden layer.
+            Matrix hidden_inputs = Matrix.DotProduct(inputs, weightInputHidden);
+            // Inputs leaving the hidden layer sigmoided.
+            Matrix hidden_outputs = hidden_inputs.Sigmoid();
+
+            // Inputs going into the final layer.
+            Matrix final_inputs = Matrix.DotProduct(hidden_outputs, weightHiddenOutput);
+            // Inputs leaving the final layer sigmoided.
+            Matrix final_outputs = final_inputs.Sigmoid();
+
+            // Errors in the output layer.
+            Matrix output_errors = target - final_outputs;
+            // Errors in the hidden layer.
+            Matrix hidden_errors = Matrix.DotProduct(output_errors, weightHiddenOutput.Transpose());
+
+            UpdateWeights(weightHiddenOutput, output_errors, final_outputs, hidden_outputs);
+            UpdateWeights(weightInputHidden, hidden_errors, hidden_outputs, inputs);
+        }
+
+        private void UpdateWeights(Matrix weights, Matrix errors, Matrix first_outputs, Matrix second_outputs)
+        {
+            Matrix temp5 = errors * first_outputs * (1.0 - first_outputs);
+            Matrix temp2 = Matrix.DotProduct(second_outputs.Transpose(), temp5);
+            for (int i = 0; i < weights.GetLength(0); i++)
+                for (int j = 0; j < weights.GetLength(1); j++)
+                    weights[i, j] += temp2[i, j] * learningrate;
+        }
+
+        static void Main(string[] args)
+        {
+            NeuralNetwork test = new NeuralNetwork(784, 100, 10, 0.3);
+            using (var reader = new System.IO.StreamReader(@"mnist_train.csv"))
+            {
+                int label;
+                while (!reader.EndOfStream)
+                {
+
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    Matrix inputs = new Matrix(1, 784);
+
+                    label = int.Parse(values[0]);
+                    for (int i = 1; i < values.Length - 1; i++)
+                    {
+                        inputs[0, i] = (int.Parse(values[i]) / 255.0 * 0.99) + 0.01;
+                    }
+                    Matrix targets = new Matrix(1, 10);
+                    for (int i = 0; i < targets.GetLength(1); i++)
+                    {
+                        targets[0, i] = 0.01;
+                    }
+                    targets[0, label] = 0.99;
+
+                    test.train(inputs, targets);
+                }
+            }
+
+            using (var reader = new System.IO.StreamReader(@"mnist_test.csv"))
+            {
+                int label;
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                Matrix inputs = new Matrix(1, 784);
+
+                label = int.Parse(values[0]);
+                for (int i = 1; i < values.Length - 1; i++)
+                {
+                    inputs[0, i] = (int.Parse(values[i]) / 255.0 * 0.99) + 0.01;
+                }
+
+                Matrix trueresults = test.Query(inputs);
+            }
+        }
+    }
+}
